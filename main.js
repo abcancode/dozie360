8c720f489075d047b5a05f172bdc50e99f75594e
abcancode <chidozieintech@gmail.com>
2025-06-11 14:40:30 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const target = document.getElementById("targetContainer");
+const element = document.getElementById("rightToLeftBox");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+observer.observe(target);
